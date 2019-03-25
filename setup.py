d27f53f702ffe3b020abda9769cd383b6b1cd2ba
Martin Åsell <martin.asell@localhost>
2019-03-25 01:02:35 +0000

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='stupid',
-    version='1.0.0',
+    version='1.0.1',
     author='masell',
     url='https://github.com/masell/stupid',
     packages=['stupid'],
